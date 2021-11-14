a01b310af9477d03c709c7adae0c9f1f61392e0e
harshjain25 <jainharsh2526@gmail.com>
2021-11-14 23:55:09 +0530

    wait and demo problem

---
+++ src/foundationB.java
@@ -0,0 +1,14 @@
+
+public class foundationB {
+    public static void main(String[] args) {
+
+
+        foundation obj = new foundation();
+        // System.out.println(obj.var1);
+        //System.out.println(obj.var2);
+        //System.out.println(obj.var3);
+        System.out.println(obj.var4);
+
+    }    }
+
+
