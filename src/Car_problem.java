bdf73dcd8a14ee769608fd60c56d1df3f8ca590b
harshjain25 <jainharsh2526@gmail.com>
2021-11-16 16:59:10 +0530

    car problem

---
+++ src/Car_problem.java
@@ -0,0 +1,20 @@
+public class Car_problem {
+    String price;
+    String manufacturer;
+    String car;
+
+    Car_problem(String p, String m, String c) {
+        System.out.println("car name =" + c);
+    }
+}
+class Class_problem
+{
+    public static void main(String[] args) {
+        Car_problem obj=new Car_problem("2k","Honda","city");
+        Car_problem obj1=new Car_problem("3k","ford","ecosport");
+        Car_problem obj2=new Car_problem("4k","maruti","breeza");
+
+
+    }
+
+}
