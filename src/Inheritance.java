162e2c203c23aca2ce37311cd6eb82bf330c73d4
harshjain25 <jainharsh2526@gmail.com>
2021-12-08 23:43:23 +0530

    Use of inheritance.

---
+++ src/Inheritance.java
@@ -0,0 +1,32 @@
+class Base{
+    int x;
+
+    public void setX(int x){
+        System.out.println("setting value of x");
+        this.x=x;
+    }
+    public int getX(){
+        return x;
+    }
+    public void printme(){
+        System.out.println("hello my name is harsh jain.");
+    }
+}
+class Derived extends Base{
+    int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+         Derived b =new Derived();
+         b.setX(4);
+         System.out.println(b.getX());
+    }
+}
