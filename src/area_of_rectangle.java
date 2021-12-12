86acda2f43e96923e7308be24ac12e34fd772dc6
harshjain25 <jainharsh2526@gmail.com>
2021-12-12 16:22:52 +0530

    area of rectangle.

---
+++ src/area_of_rectangle.java
@@ -0,0 +1,8 @@
+public class area_of_rectangle {
+    public static void main(String args[]) {
+        int width = 5;
+        int height = 10;
+        int area = width * height;
+        System.out.println("Area of rectangle=" + area);
+    }
+}
