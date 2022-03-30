5780a0f8b8b162085fe22065166dbf7bd372c568
harshjain25 <jainharsh2526@gmail.com>
2022-03-30 23:46:52 +0530

    linked list program

---
+++ src/ABLinkedList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ABLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> i=new LinkedList<>();
+        i.add(10);
+        i.add(20);
+        i.add(30);
+        i.add(40);
+        i.add(1,120);
+        System.out.println(i);
+    }
+}
---
+++ src/ABdemo2401.java
@@ -0,0 +1,14 @@
+public class ABdemo2401 {
+    public static void main(String[] args) {
+        String s="neeraj";
+        String str1=new String("abc");
+        System.out.println(s.length());
+        System.out.println(str1.length());
+
+
+        String str="neeraj";
+        int i=3;
+        String res=str.substring(0,i)+str.substring(i+1);
+        System.out.println(res);
+    }
+}
