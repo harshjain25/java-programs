8004988a9b5a626915c629f0abdd65d21791f046
harshjain25 <jainharsh2526@gmail.com>
2023-01-12 13:33:39 +0530

    java practice programs

--- src/A3.java
+++ src/A3.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 class A3{
-    static{
-        System.out.println("static block is invoked");
-        System.exit(0);}
+    public static void print(int i,int n){
+        if(i>n){
+            return;
+        }
+        System.out.println("harsh");
+        print(i+1,n);
+    }
     public static void main(String[] args) {
-
-
-
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=1;
+        print(i,n);
     }
 }
---
+++ src/abc.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class abc {
+
+        public static void main(String[] args) {
+            int[] x = new int[10];
+            for (int P = 0; P <=x.length; P++) {
+                x[P] = 5;
+            }
+        }
+    }
+
