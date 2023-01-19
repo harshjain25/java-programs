fead9acd307291302c9c675192f9baf8e19f62ee
harshjain25 <jainharsh2526@gmail.com>
2023-01-19 14:14:26 +0530

    creating tree from level order traversal

---
+++ src/createLevelOrder.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class createLevelOrder {
+    private Node root;
+    Scanner sc=new Scanner(System.in);
+    public CreateLevelOrder(){
+        this.root=CreateTree();
+
+    }
+    private Node CreateTree(){
+        Queue<Node> q=new LinkedList<>();
+        Node nn=new Node();
+        int val=sc.nextInt();
+        nn.val=val;
+        root =nn;
+        q.add(nn);
+        while(!q.isEmpty()){
+            Node rv=q.remove();
+            int c1=sc.nextInt();
+            int c2=sc.nextInt();
+            if(c1!=-1){
+                Node node=new Node();
+                node.val=c1;
+                rv.left=node;
+                q.add(node);
+            }
+            if(c2!=-1){
+                Node node=new Node();
+                node.val=c2;
+                rv.right=node;
+                q.add(node);
+            }
+        }
+        return root;
+    }
+}
